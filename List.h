531567d33f994a2a525bb4a2c824d2efd493ad96
PVAYGIN <pvaygin@alumno.upv.es@ldsic-vdi03.upvnet.upv.es>
2026-01-02 10:59:15 +0100

    Añadida interfaz genérica List

--- List.h
+++ List.h
@@ -4,11 +4,17 @@
 template <typename T> 
 class List {
     public:
-        virtual ~List() {}
 
-	virtual void insert(int pos, T e) = 0;
+	virtual void insert (int pos, T e) = 0;	
+	virtual void append (T e) = 0;
+	virtual void prepend (T e) = 0;
+	virtual T remove (int pos) = 0;
+	virtual T get (int pos)  const = 0;
+	virtual int search(T e)  const = 0;
+	virtual bool empty()  const = 0;
+	virtual int  size() const = 0; 
 
-
+	
 
 
 
