01393a55e9f35ccba35301c5f3c9476ceda8a1b8
Pau Vaya Giner <pvaygin@upv.edu.es>
2026-01-02 12:27:58 +0100

    hola primo

--- List.h
+++ List.h
@@ -1,9 +1,11 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdexcept>
 template <typename T> 
 class List {
     public:
+	virtual  ~List() {}
 
 	virtual void insert (int pos, T e) = 0;	
 	virtual void append (T e) = 0;
